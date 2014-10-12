62646ac6350d70b19e28d32e93c77aaa9cbd0d48
arunpatro <arun.kumar.patro@gmail.com>
2014-10-12 21:33:01 +0000

    Complete point operations including Otsu thresholding

--- hist.py
+++ hist.py
@@ -3,36 +3,14 @@ from scipy.misc import imread
 import matplotlib.pyplot as plt
 lenag=imread('lenag.bmp')
 
+a=np.arange(0,256)
+pixels=np.array([0 for x in range(256)])
+for i in range(0,512):
+	for j in range(0,512):
+		bright=lenag[i,j]
+		pixels[bright]=pixels[bright]+1
 
-def histo(image,label):
-	a=np.arange(0,256,1)
-	b=[0]
-	c=[0]
+histo, bins =np.histogram(lenag, bins=range(255))
 
-	for i in range(0,255):
-		b=b+c
-	pixels=np.array(b)
-	for bright in a:
-		pixels[bright]=0
-	for x in range(0,512):
-		for y in range(0,512):
-			bright=image[x,y]
-			pixels[bright]=pixels[bright]+1
-	plt.plot(a,pixels)
-	plt.xlabel(label)
-	plt.show()
-	return
-
-histo(lenag,'Normal Histogram')
-
-lenagN=((lenag-25)*(256/175))
-
-histo(lenagN,'Normalized Histogram')
-
-
-
-
-
-
-
-
+plt.plot(a,pixels)
+plt.show()
