bf9875a592b778addda58370e55ff7187eec2238
krishna95 <krishna.bagadia2@gmail.com>
2014-10-13 23:42:34 +0530

    Implementation

--- equalisation.py
+++ equalisation.py
@@ -6,20 +6,28 @@ panda = imread('lena.bmp')
 def rgb2grey(image):
     grey =np.floor(np.mean (image,axis = 2))
     return grey
-g = rgb2grey(panda)
-bright_sum=hist= np.zeros((256,1))
-psum=0
-rows,cols,channels = panda.shape
-for i in range(0,rows):
-    for j in range(0,cols):
-        bright_sum[g[i][j]]=bright_sum[g[i][j]] + 1
-number_of_pixels=rows*cols
-for i in range(256):
-    psum =bright_sum[i] + psum
-    hist[i]=np.floor(255*1.0*psum)/number_of_pixels
-new = np.zeros((rows,cols))
-for i in range(rows):
-    for j in range(cols):
-        new[i][j]=hist[g[i][j]]
-plt.hist(new.flatten())
-plt.show()
+
+def equalise(image):
+	rows, cols, channels = image.shape
+	if (channels == 3):
+		image = rgb2grey(image)
+	bright_sum = hist = np.zeros(256)
+	psum=0
+	for i in range(0,rows):
+		for j in range(0,cols):
+			bright_sum[image[i][j]]=bright_sum[image[i][j]] + 1
+	number_of_pixels=rows*cols
+	for i in range(256):
+		psum =bright_sum[i] + psum
+		hist[i]=np.floor(255*1.0*psum)/number_of_pixels
+	new = np.zeros((rows,cols))
+	for i in range(rows):
+		for j in range(cols):
+			new[i][j]=hist[image[i][j]]
+	return new
+
+if __name__ == "__main__":
+	panda = imread('panda.jpg')
+	grey = equalise(panda)
+	plt.imshow(grey,cmap=cm.Greys_r)
+	plt.show()
--- normalisation.py
+++ normalisation.py
@@ -1,25 +1,22 @@
 import numpy as np
-%matplotlib inline
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm 
 from scipy.misc import imread
 panda=imread('panda.jpg')
-plt.imshow(panda)
-plt.imshow(panda)
-rows,cols,channels = panda.shape
-print(panda.shape)
-amax = 0
-amin = 255
-grey = np.mean(panda,axis=2)
-#for i in range(rows):
- #   for j in range(cols):
-  #      if (amax > panda[i,j,0]):
-   #         amax = panda[i,j,0]
-    #    if (amin < panda[i,j,0]):
-     #      amin = panda[i,j,0]
-amax=np.amax(grey)
-amin=np.amin(grey)
-plt.imshow(grey,cmap=cm.Greys_r)
-grey[:rows,:cols]=(255-0)/(amax-amin)*grey[:rows,:cols]+0
-print(grey.flatten())
-plt.hist(grey.flatten(),normed=True)
+
+def normalise(image):
+	rows,cols,channels = image.shape
+	amax = 0
+	amin = 255
+	if (channels == 3):
+		grey = np.mean(image,axis=2)
+	amax=np.amax(grey)
+	amin=np.amin(grey)
+	grey[:rows,:cols]=(255-0)/(amax-amin)*grey[:rows,:cols]+0
+	return grey
+
+if __name__ == "__main__":
+	panda = imread('panda.jpg')
+	grey=normalise(panda)
+	plt.imshow(grey,cmap=cm.Greys_r)
+	plt.show()
