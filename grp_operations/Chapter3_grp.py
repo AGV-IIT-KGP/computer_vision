e61cbb7b686a122c48943bbed09df6e26ab7f462
arna <arnatubai@arnatubai-HP-Pavilion-dv6-Notebook-PC.(none)>
2014-10-24 03:26:27 +0530

    implemeted successfully till mode-filter(Section 3.5) but used loops

--- grp_operations/Chapter3_grp.py
+++ grp_operations/Chapter3_grp.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import scipy.signal as sig
 import numpy as np
 
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.557, 0.144])
+
 def normalisation(gray,nmin,nmax):
 	omax=np.amax(lena)
 	omin=np.amin(lena)
@@ -34,8 +37,69 @@ def gaussian_avg(A,win_size,sigma):
 	#print(B)
 	return convolution(A,B)		
 
+def median_filter(A,win_size):
+	rows,cols=A.shape
+	C=A.copy()
+	B=np.zeros(win_size*win_size)
+	for i in range(rows-1):
+		for j in range(cols-1):
+			count=0
+			if(i>(win_size/2)-1 and j>(win_size/2)-1):
+				for k in range(win_size/2):
+					for l in range(win_size/2):
+						B[count]=A[i+k,j+l]
+						count=count+1
+						B[count]=A[i-k,j-l]
+						count=count+1
+						B[count]=A[i+k,j-l]
+						count=count+1
+						B[count]=A[i-k,j+l]
+						count=count+1
+				C[i,j]=np.median(B)
+	return C			
+	
+def mode_filter(A,win_size):
+	rows,cols=A.shape
+	C=A.copy()
+	B=np.zeros(win_size*win_size)
+	for i in range(rows-1):
+		for j in range(cols-1):
+			count=0
+			if(i>(win_size/2)-1 and j>(win_size/2)-1):
+				#print(win_size/2)
+				for k in range(win_size/2):
+					for l in range(win_size/2):
+						
+						B[count]=A[i+k,j+l]
+						count=count+1
+						B[count]=A[i-k,j-l]
+						count=count+1
+						B[count]=A[i+k,j-l]
+						count=count+1
+						B[count]=A[i-k,j+l]
+						count=count+1
+				med=np.median(B)
+				mean=np.mean(B)
+				lower=2*med-np.max(B)
+				upper=2*med-np.min(B)
+				ListD=[]
+				listCount=0
+				for k in range(count):
+					if(B[k]<upper and med<mean):
+						ListD.append(B[k])
+						listCount=listCount+1
+					if(B[k]>lower and med>mean):
+						ListD.append(B[k])
+						listCount=listCount+1
+				ArrayE=np.asarray(ListD)
+				if(listCount>0):
+					C[i,j]=np.median(ArrayE)	
+				else: C[i,j]=med
+	return C				
+
 if __name__=="__main__":
-	lena=misc.imread("lena.png")
+	lena=misc.imread("geoaware1.jpg")
+	lena=rgb2gray(lena)
 	rows,cols=lena.shape
 	print(rows,cols)
 	#convolution
@@ -50,4 +114,12 @@ if __name__=="__main__":
 	#gaussian averaging
 	gaussian_lena=gaussian_avg(lena,5,1)
 	plt.imshow(gaussian_lena,cmap=plt.cm.gray)
+	plt.show()
+	#median filter
+	med_lena=median_filter(lena,5)
+	plt.imshow(med_lena,cmap=plt.cm.gray)
+	plt.show()
+	#mode filter
+	mod_lena=mode_filter(lena,5)
+	plt.imshow(mod_lena,cmap=plt.cm.gray)
 	plt.show()
