cea62e22090f6eed0eaab2d87163cb8460594ed5
Harsh Gupta <gupta.harsh96@gmail.com>
2014-11-03 23:29:33 +0530

    fix travis setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+from distutils.core import setup
+setup(name='agv_cv',
+      version='0.1',
+      packages=['agv_cv'],
+      install_requires=[
+          'scipy',
+          'numpy',
+      ],
+      )
