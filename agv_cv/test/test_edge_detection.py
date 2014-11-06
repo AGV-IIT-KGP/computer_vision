ac35d1f1cf1342ca1a78287fec99895f17c24940
krishna95 <krishna.bagadia2@gmail.com>
2014-11-06 12:58:26 +0530

    Changed the output image for assert equal

--- agv_cv/test/test_edge_detection.py
+++ agv_cv/test/test_edge_detection.py
@@ -3,8 +3,8 @@ from agv_cv.edge_detection import edgeDetection
 from numpy.testing import assert_equal, assert_almost_equal
 from scipy.misc import imread
 
-test = imread('../../lena.bmp')
-result = imread('../../test_edge.png')
+test = imread('../../images/lena.bmp')
+result = imread('../../images/test_edge.png')
 
 def test_edge_detection():
 	assert_equal(edgeDetection(test),result)
